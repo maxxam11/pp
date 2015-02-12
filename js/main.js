6a072101feb8cfb696c59301eb839ef8b19d6457
maxxam11 <karachev.m@gmail.com>
2015-02-12 17:41:47 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var global = {
+    spaceId: 1,
+    timeout: {
+        noInternet: 10000,
+        loadStart: 0,
+    },
+};
+
+function goToIndex() {
+    document.location.href = '/';
+}
+function gotoError(text) {
+    document.location.href = '#!/error/' + text + '/';
+}
+function gotoErrorNoInternet() {
+    document.location.href = '#!/nointernet';
+}
+function getToken() {
+    return localStorage.token ? localStorage.token : '';
+}
+function setToken(token) {
+    localStorage.setItem('token', token);
+}
