e878e82825f63adbbb1d4e7d6708ee14f0f96cf9
maxxam11 <karachev.m@gmail.com>
2015-02-27 16:47:32 +0400

    версия api

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ var global = {
     },    
     api: {
         //url: 'http://pp.maxxam.ru/api/',
-        url: 'http://127.0.0.1:8000/api/',       
+        url: 'http://127.0.0.1:8000/api/', 
+        version: 1,
     },
     timeout: {
         noInternet: 10000,
