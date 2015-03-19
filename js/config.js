cc7d1c1f90d1d83e7a74c51607dcf6b72b1953ef
maxxam11 <karachev.m@gmail.com>
2015-03-19 20:36:31 +0400

    перешел на post запросы и вывел новости

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var global = {
     },    
     api: {
         //url: 'http://pp.maxxam.ru/api/',
-        url: 'http://127.0.0.1:8000/api/', 
+        url: 'http://178.62.225.181/api/', 
         version: 1,
         token: '34829a123b98d99c',
     },
