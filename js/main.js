a79e486eb1badc4e60178c75cc69e4dff8ebf10e
maxxam11 <karachev.m@gmail.com>
2015-05-27 20:52:56 +0400

    детектор мобилы

--- js/main.js
+++ js/main.js
@@ -45,3 +45,22 @@ function getToken() {
 function setToken(token) {
     localStorage.setItem('token', token);
 }
+
+/**
+ * Определяет, на мобиле мы или нет
+ * @returns {Boolean}
+ */
+function detectmob() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       return true;
+    } else {
+       return false;
+    }
+}
